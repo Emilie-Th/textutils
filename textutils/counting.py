7b11ae28a05720da814240e941fbaeb3b26aae09
Emilie-Th <emilie.theotime@gmail.com>
2026-09-25 23:27:24 +0200

    Add include_spaces option to character_count(text)

--- textutils/counting.py
+++ textutils/counting.py
@@ -13,15 +13,22 @@ def word_count(text):
     
 
 
-def character_count(text):
+def character_count(text, include_spaces=True):
     """
-    Count the number of characters in a text, including spaces.
+    Count the number of characters in a text.
 
     Args:
     text (str): The text in which to count the characters.
+    include_spaces (bool): Whether to include spaces in the count. Defaults to True.
 
     Returns:
     int: The number of characters in the text.
     """
-    return len(text)
-
+    if not include_spaces:
+        nb = 0
+        for i in range(len(text)):
+            if text[i] != " ":
+                nb += 1
+        return nb 
+    else:
+        return len(text)
