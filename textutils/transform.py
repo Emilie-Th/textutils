b3ff17a50d5a4a17695fb289979ff6f71a63d17e
Emilie-Th <emilie.theotime@gmail.com>
2026-09-25 23:38:07 +0200

    Implement reverse(text)

---
+++ textutils/transform.py
@@ -0,0 +1,11 @@
+def reverse(text):
+    """
+   Reverse the characters in a text.
+
+   Args:
+       text (str): The text to reverse.
+
+   Returns:
+       str: The reversed text.
+   """
+    return text[::-1]
